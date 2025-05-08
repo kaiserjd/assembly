2f821e0f28f39675e3a4652fd0a3d48cd14e5e00
kaiserjd03 <kaiserjd03@gmail.com>
2025-05-08 19:02:45 -0400

    various scripts

---
+++ helloworld/helloworld.s
@@ -0,0 +1,16 @@
+global _start
+
+section .data
+    message db "Hello World!"
+
+section .text
+_start:
+    mov rsi, message
+    mov rdi, 1
+    mov rdx, 18
+    mov rax, 1
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
