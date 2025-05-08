5e234498d3010e78e111205a80ea5a5d9c9d0cc1
kaiserjd <kaiserjd03@gmail.com>
2025-05-07 20:47:56 -0500

    final fib

---
+++ assemble2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+fileName="${1%%.*}" # remove .s extension
+
+nasm -f elf64 ${fileName}".s"
+ld ${fileName}".o" -o ${fileName} -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2
+[ "$2" == "-g" ] && gdb -q ${fileName} || ./${fileName}
--- fibonacci.s
+++ fibonacci.s
@@ -1,13 +1,18 @@
 global _start
-extern printf
+extern printf, scanf
 
 section .data
-    message db "Fibonacci Sequence:", 0x0a
+    message db "Please input max Fn:", 0x0a
     outFormat db "%d", 0x0a, 0x00
+    inFormat db "%d", 0x00
+
+section .bss
+    userInput resb 1; reserve 1 byte of buffer space
 
 section .text
 _start:
     call printMessage
+    call getInput
     call initFib
     call loopFib
     call Exit
@@ -18,6 +23,16 @@ printMessage:
     mov rsi, message; rsi: pointer to message
     mov rdx, 20     ; rdx: print length of 20 bytes
     syscall         ; call the write syscall for our intro message
+    ret
+
+getInput:
+    sub rsp, 8          ; align stack to 16-byte boundary
+
+    mov rdi, inFormat   ; 1st parameter (inFormat)
+    mov rsi, userInput  ; 2nd parameter (userInput)   
+    call scanf
+
+    add rsp, 8
     ret
 
 printFib:
@@ -39,11 +54,12 @@ initFib:
     ret
 
 loopFib:
-    add rax, rbx    ; get next fibonacci number
-    xchg rax, rbx   ; swap rax and rbx
+    call printFib           ; print current fib number
+    add rax, rbx            ; get next fibonacci number
+    xchg rax, rbx           ; swap rax and rbx
 
-    cmp rbx, 10     ; do rbx - 10
-    js loopFib      ; jump if result < 0
+    cmp rbx, [userInput]    ; do rbx - userInput
+    js loopFib              ; jump if result < 0
     ret
 
 Exit:
